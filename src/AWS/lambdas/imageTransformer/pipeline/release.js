4e92284b766b95a03614a27fd15600b2b5866b25
Attila Bartha <atikenny@gmail.com>
2021-10-31 18:36:50 +0100

    update lambda function release

--- src/AWS/lambdas/imageTransformer/pipeline/release.js
+++ src/AWS/lambdas/imageTransformer/pipeline/release.js
@@ -1,24 +1,35 @@
-const { LambdaClient, UpdateFunctionCodeCommand } = require('@aws-sdk/client-lambda');
+const {
+  LambdaClient,
+  UpdateFunctionCodeCommand,
+  UpdateFunctionConfigurationCommand
+} = require('@aws-sdk/client-lambda');
 const { readFileSync } = require('fs');
 
 const region = 'us-west-2';
 
 const lambda = new LambdaClient({ region });
 
-// Set the parameters.
-const params = {
-  FunctionName: 'imageTransformer',
-  Handler: 'index.handler',
-  Runtime: 'nodejs14.x',
-  ZipFile: readFileSync('./build.zip')
-};
-
-const run = async () => {
+const release = async () => {
   try {
-    const data = await lambda.send(new UpdateFunctionCodeCommand(params));
-    console.log('Success', data); // successful response
+    const updateConfigParams = {
+      FunctionName: 'imageTransformer',
+      Handler: 'src/index.handler',
+      Runtime: 'nodejs14.x'
+    };
+
+    const configData = await lambda.send(new UpdateFunctionConfigurationCommand(updateConfigParams));
+    console.log('Success', configData); // eslint-disable-line no-console
+
+    const updateCodeParams = {
+      FunctionName: 'imageTransformer',
+      ZipFile: readFileSync('./build.zip')
+    };
+
+    const codeData = await lambda.send(new UpdateFunctionCodeCommand(updateCodeParams));
+    console.log('Success', codeData); // eslint-disable-line no-console
   } catch (err) {
-    console.log('Error', err); // an error occurred
+    console.log('Error', err); // eslint-disable-line no-console
   }
 };
-run();
+
+release();
