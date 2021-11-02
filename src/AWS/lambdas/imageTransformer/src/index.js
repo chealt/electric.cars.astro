8eeb7103252e194fd334e38a4b901e754172927b
Attila Bartha <atikenny@gmail.com>
2021-11-02 07:58:11 +0100

    add more logging, upload image to S3

--- src/AWS/lambdas/imageTransformer/src/index.js
+++ src/AWS/lambdas/imageTransformer/src/index.js
@@ -3,22 +3,47 @@ const aws = require('aws-sdk');
 const { ImagePool } = require('@squoosh/lib');
 const { cpus } = require('os');
 
-const imagePool = new ImagePool(cpus().length);
+const numberOfCPUs = cpus().length;
+console.log(`Number of CPUs: ${numberOfCPUs}.`);
+const imagePool = new ImagePool(numberOfCPUs);
 
 const s3 = new aws.S3();
 
-const loadImage = async ({ bucket, key }) => {
+const loadFile = async ({ bucket, key }) => {
   const params = {
     Bucket: bucket,
     Key: key
   };
 
   try {
+    console.log('Loading file from S3...');
     const object = await s3.getObject(params).promise();
+    console.log('Loaded file from S3.');
 
     return object.Body;
   } catch (error) {
-    console.error(`Cannot load image from S3, bucket: ${bucket}, key: ${key}`);
+    console.error(`Cannot load file from S3, bucket: ${bucket}, key: ${key}`);
+
+    throw error;
+  }
+};
+
+const uploadFile = async ({ bucket, key, binary }) => {
+  const params = {
+    ACL: 'bucket-owner-full-control',
+    Bucket: bucket,
+    Key: key,
+    Body: binary
+  };
+
+  try {
+    console.log('Uploading file to S3...');
+    const object = await s3.putObject(params).promise();
+    console.log('Uploaded file to S3.');
+
+    return object.Body;
+  } catch (error) {
+    console.error(`Cannot upload file to S3, bucket: ${bucket}, key: ${key}`);
 
     throw error;
   }
@@ -29,8 +54,7 @@ exports.handler = async (event) =>
     event.Records.map(async (record) => {
       const bucket = record.s3.bucket.name;
       const key = decodeURIComponent(record.s3.object.key.replace(/\+/gu, ' '));
-      const imageBuffer = await loadImage({ bucket, key });
-      console.log('Loaded image from S3.');
+      const imageBuffer = await loadFile({ bucket, key });
 
       const image = imagePool.ingestImage(imageBuffer);
       await image.decoded;
@@ -47,9 +71,15 @@ exports.handler = async (event) =>
 
       const encodeOptions = {
         webp: 'auto'
+        // mozjpeg: 'auto'
       };
       await image.encode(encodeOptions);
       console.log('Image encoded.');
+
+      const rawEncodedImage = await image.encodedWith.webp;
+      const newImageExtension = rawEncodedImage.extension;
+      const newKey = `${key.slice(0, key.lastIndexOf('.'))}.${newImageExtension}`;
+      await uploadFile({ bucket, key: newKey, binary: Buffer.from(rawEncodedImage.binary, 'binary') });
 
       console.log('Success');
     })
