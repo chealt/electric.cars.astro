e390b7f55d3ce54d9eb06f066b9af95f0cac6846
Attila Bartha <atikenny@gmail.com>
2021-12-13 11:09:33 +0100

    use globs to upload images

--- src/utils/imageTransform.mjs
+++ src/utils/imageTransform.mjs
@@ -1,16 +1,19 @@
 import { exit } from 'process';
 import fs from 'fs';
+import glob from 'glob';
 import { promisify } from 'util';
 import { transformImage } from './image/index.mjs';
 import { uploadFile } from './S3/index.mjs';
 
 const readFile = promisify(fs.readFile);
+const globAsync = promisify(glob);
+const getImages = (folder) => globAsync(`${folder}/**/*.+(jpg|jpeg|webp|png)`);
 const getConfig = () => {
-  const filename = process.env.FILENAME;
+  const folder = process.env.FOLDER;
   const bucket = process.env.AWS_BUCKET;
 
-  if (!filename) {
-    throw new Error('Must provide a FILENAME env var.');
+  if (!folder) {
+    throw new Error('Must provide a FOLDER env var.');
   }
 
   if (!process.env.AWS_ACCESS_KEY_ID) {
@@ -26,14 +29,14 @@ const getConfig = () => {
   }
 
   return {
-    filename,
+    folder,
     AWS: { bucket },
     widths: [300, 600, 800, 'original']
   };
 };
 
 const {
-  filename,
+  folder,
   AWS: { bucket },
   widths
 } = getConfig();
@@ -41,17 +44,23 @@ const {
 await Promise.all(
   widths.map(async (width) => {
     const format = 'webp';
-    const imageBuffer = await readFile(filename);
-    const transformedImage = await transformImage({ buffer: imageBuffer, format, width });
-    const newKey = `${filename.slice(0, filename.lastIndexOf('.'))}-${width}.${transformedImage.extension}`;
+    const files = await getImages(folder);
 
-    console.log(`File will be created with the filename: ${newKey}`);
+    return Promise.all(
+      files.map(async (filename) => {
+        const imageBuffer = await readFile(filename);
+        const transformedImage = await transformImage({ buffer: imageBuffer, format, width });
+        const newKey = `${filename.slice(0, filename.lastIndexOf('.'))}-${width}.${transformedImage.extension}`;
 
-    const uploadPromise = uploadFile({ bucket, key: newKey, binary: transformedImage.buffer });
+        console.log(`File will be created with the filename: ${newKey}`);
 
-    console.log(`Image uploaded with width: ${width}, and format: ${format}`);
+        const uploadPromise = uploadFile({ bucket, key: newKey, binary: transformedImage.buffer });
 
-    return uploadPromise;
+        console.log(`Image uploaded with width: ${width}, and format: ${format}`);
+
+        return uploadPromise;
+      })
+    );
   })
 );
 
